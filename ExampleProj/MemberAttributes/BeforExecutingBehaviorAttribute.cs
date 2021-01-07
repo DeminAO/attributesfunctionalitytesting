94f03ea6909edc97e0dd64d811b6d5920dbf8c2a
DeminAORostov <57270583+DeminAO@users.noreply.github.com>
2021-01-07 09:53:40 +0300

    *

--- ExampleProj/MemberAttributes/BeforExecutingBehaviorAttribute.cs
+++ ExampleProj/MemberAttributes/BeforExecutingBehaviorAttribute.cs
@@ -5,7 +5,7 @@ namespace ExampleProj.MethodCallTracing
 	/// <summary>
 	/// Назначает метод, выполнчющийсч до вызова метода, на который данный атрибут повешен
 	/// </summary>
-	public class BeforExecutingBehaviorAttribute : ActionNameAttribute
+	public sealed class BeforExecutingBehaviorAttribute : ActionNameAttribute
 	{
 		/// <param name="ActionName">Наименование метода без параметров длч вызова</param>
 		public BeforExecutingBehaviorAttribute(string ActionName)
